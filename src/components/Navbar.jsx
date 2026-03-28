13f8bccd23640c961df7e3d62510d8e726ae4684
Virshree <virshreedesai19@gmail.com>
2026-03-28 11:59:58 +0530

    responsive code done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,100 +1,158 @@
-import React, { useState } from 'react'
-import logo from '../images/logo.svg';
-import avatar from '../images/image-avatar.png';
-import cart from '../images/icon-cart.svg';
+import React, { useState } from "react";
+import logo from "../images/logo.svg";
+import avatar from "../images/image-avatar.png";
+import cart from "../images/icon-cart.svg";
 import product1 from "../images/image-product-1-thumbnail.jpg";
 import deleteIcon from "../images/icon-delete.svg";
+import menuIcon from "../images/icon-menu.svg";
+import closeIcon from "../images/icon-close.svg";
 
+function Navbar({ cartQuantity, setCartQuantity }) {
+  const [cartOpen, setCartOpen] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
-function Navbar({cartQuantity,setCartQuantity}) {
-  const [cartOpen,setCartOpen]=useState(false);
-  function handleCart(){
-    setCartOpen(!cartOpen);
-  }
   return (
-    <div className='flex m-5 p-4 ml-50 border-b  border-b-slate-300'>
-        <img src={logo} alt="logo" className='w-[150px] h-[30px]'/>
-        <div className="flex gap-10 ml-20 text-[hsl(220,14%,75%)] h-full">
-  {["Collections", "Men", "Women", "About", "Contact"].map((item, i) => (
-    <span
-      key={i}
-      className="flex items-center h-full cursor-pointer 
-      border-b-2 border-transparent
-      hover:text-[hsl(219,9%,45%)] 
-      hover:border-orange-500 transition-all duration-200"
-      
-    >
-      {item}
-    </span>
-  ))}
-</div>
-        <div className='flex ml-110 gap-5 cursor-pointer '>
-            <img src={cart} alt="cart-icon" className='w-[40px]  h-[37px] p-2 'onClick={handleCart}/>
-            
-            {cartQuantity > 0 && (
-    <span className="absolute ml-7 bg-orange-500 text-white text-xs px-2 rounded-full">
-      {cartQuantity}
-    </span>
-  )}
-         <div className='w-[300px] h-[540px]  mt-10 absolute'> 
-         {cartOpen && (
-  <div className="bg-white shadow-lg rounded-xl w-[320px]">
-    <h2 className="font-black p-3">Cart</h2>
-          <hr/>
-    {cartQuantity > 0 ? (
-   <div className="p-3 w-[280px]">
-  
-   <div className="flex items-center justify-between">
-     
-     <div className="flex gap-2">
-       <img
-         src={product1}
-         alt="product"
-         className="w-12 rounded-md"
-       />
- 
-       <div>
-         <p className="text-gray-400 text-[14px]">
-           Fall Limited Edition Sneakers
-         </p>
- 
-         <p className="text-gray-400 text-[13px]">
-           $125.00 x {cartQuantity}{" "}
-           <span className="text-black font-bold">
-             ${125 * cartQuantity}.00
-           </span>
-         </p>
-       </div>
-     </div>
- 
-   
-   </div>
-   <img
-       src={deleteIcon}
-       alt="delete"
-       className="w-4  flex ml-65 mt-[-30px]  cursor-pointer"
-       onClick={() => setCartQuantity(0)}
-     />
-   <button  onClick={()=>setCartQuantity(0)} className="bg-[hsl(26,100%,55%)] cursor-pointer  mt-7 w-[290px] py-3 rounded-lg font-bold">
-     Checkout
-   </button>
- </div>
+    <>
+      {/* NAVBAR */}
+      <div className="border-b border-gray-200 mx-auto max-w-[1200px]">
+        <div className="max-w-[1200px] mx-auto flex items-center justify-between py-4 px-4">
+          {/* LEFT */}
+          <div className="flex items-center gap-4 md:gap-10">
+            <img
+              src={menuIcon}
+              className="w-5 md:hidden cursor-pointer"
+              onClick={() => setShowMenu(true)}
+            />
 
+            <img src={logo} className="w-[120px] md:w-[140px]" />
 
-      
-      
-    ) : (
-      <p className="text-[hsl(220,14%,75%)] m-4 p-5 text-center  h-[100px]">
-        Your cart is empty.
-      </p>
-    )}
-  </div>
-)}
-            </div> 
-            <img src={avatar} alt="avatar" className='w-[40px] h-[37px]'/>
+            {/* DESKTOP MENU */}
+            <div className="hidden md:flex gap-8 text-gray-400 mt-6">
+              {["Collections", "Men", "Women", "About", "Contact"].map(
+                (item, i) => (
+                  <span
+                    key={i}
+                    className="cursor-pointer pb-6 border-b-2 border-transparent hover:border-orange-500 hover:text-black"
+                  >
+                    {item}
+                  </span>
+                )
+              )}
+            </div>
+          </div>
+
+          {/* RIGHT */}
+          <div className="flex items-center gap-5 relative">
+            {/* CART */}
+            <div className="relative">
+              <img
+                src={cart}
+                className="w-6 cursor-pointer"
+                onClick={() => setCartOpen(!cartOpen)}
+              />
+
+              {cartQuantity > 0 && (
+                <span className="absolute -top-2 -right-2 bg-orange-500 text-white text-xs px-2 rounded-full">
+                  {cartQuantity}
+                </span>
+              )}
+            </div>
+
+            {/* CART DROPDOWN */}
+            {cartOpen && (
+              <div className="fixed inset-0 z-50 flex justify-center items-start pt-16 md:justify-end md:pt-24">
+                <div
+                  className="absolute inset-0 bg-black/50 md:hidden"
+                  onClick={() => setCartOpen(false)}
+                />
+
+                <div className="relative w-[92%] max-w-[360px] md:w-[360px] bg-white rounded-xl shadow-2xl md:mr-10">
+                  <h2 className="font-bold p-5">Cart</h2>
+                  <hr />
+
+                  {cartQuantity > 0 ? (
+                    <div className="p-5">
+                      <div className="flex justify-between items-center">
+                        <div className="flex gap-3">
+                          <img src={product1} className="w-12 rounded-md" />
+                          <div>
+                            <p className="text-gray-400 text-sm">
+                              Fall Limited Edition Sneakers
+                            </p>
+                            <p className="text-gray-400 text-sm">
+                              $125 x {cartQuantity}{" "}
+                              <span className="text-black font-bold">
+                                ${125 * cartQuantity}
+                              </span>
+                            </p>
+                          </div>
+                        </div>
+
+                        <img
+                          src={deleteIcon}
+                          className="w-4 cursor-pointer"
+                          onClick={() => setCartQuantity(0)}
+                        />
+                      </div>
+
+                      <button
+                        onClick={() => setCartQuantity(0)}
+                        className="bg-orange-500 w-full py-3 mt-6 rounded-lg text-white font-bold cursor-pointer"
+                      >
+                        Checkout
+                      </button>
+                    </div>
+                  ) : (
+                    <div className="flex items-center justify-center h-[150px]">
+                      <p className="text-gray-400">Your cart is empty.</p>
+                    </div>
+                  )}
+                </div>
+              </div>
+            )}
+
+            {/* AVATAR */}
+            <img
+              src={avatar}
+              className="w-9 md:w-10 rounded-full cursor-pointer border-2 border-transparent hover:border-orange-500"
+            />
+          </div>
         </div>
-    </div>
-  )
+      </div>
+
+      {/* MOBILE MENU */}
+      <div
+        className={`fixed inset-0 z-50 flex ${
+          showMenu ? "visible" : "invisible"
+        }`}
+      >
+        <div
+          className={`w-[70%] max-w-[260px] bg-white p-6 transform transition ${
+            showMenu ? "translate-x-0" : "-translate-x-full"
+          }`}
+        >
+          <img
+            src={closeIcon}
+            onClick={() => setShowMenu(false)}
+            className="mb-8 cursor-pointer"
+          />
+
+          <div className="flex flex-col gap-6 font-bold text-lg">
+            {["Collections", "Men", "Women", "About", "Contact"].map(
+              (item, i) => (
+                <span key={i}>{item}</span>
+              )
+            )}
+          </div>
+        </div>
+
+        <div
+          className="flex-1 bg-black/60"
+          onClick={() => setShowMenu(false)}
+        />
+      </div>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,195 +1,210 @@
 import React, { useState } from "react";
-
 import minus from "../images/icon-minus.svg";
 import plus from "../images/icon-plus.svg";
 import cart from "../images/icon-cart.svg";
+
 import shoe1 from "../images/image-product-1.jpg";
 import shoe2 from "../images/image-product-2.jpg";
 import shoe3 from "../images/image-product-3.jpg";
 import shoe4 from "../images/image-product-4.jpg";
+
 import product1 from "../images/image-product-1-thumbnail.jpg";
 import product2 from "../images/image-product-2-thumbnail.jpg";
 import product3 from "../images/image-product-3-thumbnail.jpg";
 import product4 from "../images/image-product-4-thumbnail.jpg";
 
-import closeIcon from "../images/icon-close.svg";
 import next from "../images/icon-next.svg";
 import prev from "../images/icon-previous.svg";
+import closeIcon from "../images/icon-close.svg";
 
 function Product({ setCartQuantity }) {
   const [quantity, setQuantity] = useState(0);
-
-  const [mainImage, setMainImage] = useState(shoe1);
   const [selectedIndex, setSelectedIndex] = useState(0);
   const [showLightbox, setShowLightbox] = useState(false);
 
-  const bigImages = [shoe1, shoe2, shoe3, shoe4];
+  const images = [shoe1, shoe2, shoe3, shoe4];
   const thumbnails = [product1, product2, product3, product4];
+
   return (
-    <div className="flex mt-20 p-4 ml-60 gap-30">
-      <div className="flex gap-10">
-        <img
-          src={mainImage}
-          alt="shoe-1"
-          onClick={() => setShowLightbox(true)}
-          className="w-[420px] h-[450px] rounded-xl"
-        />
-      </div>
-      {showLightbox && (
-        <div className="fixed inset-0 bg-black/70 flex flex-col items-center justify-center z-50">
-          {/* Close button */}
+    <div className="max-w-[1200px] mx-auto  flex flex-col md:flex-row gap-10 md:mt-16 px-0 md:px-6">
+      {/* LEFT SIDE (IMAGE) */}
+      <div className="w-full md:w-1/2">
+        {/* MAIN IMAGE */}
+        <div className="relative ">
           <img
-            src={closeIcon}
-            alt="close-btn"
-            onClick={() => setShowLightbox(false)}
-            className="absolute top-25 right-152 hover:bg-orange-400 cursor-pointer "
+            src={images[selectedIndex]}
+            onClick={() => setShowLightbox(true)}
+            className="w-[450px] h-[330px] object-cover md:h-[450px] md:rounded-xl cursor-pointer"
           />
+          {showLightbox && (
+            <div className="fixed inset-0 bg-black/70 z-50 flex items-center justify-center">
+              {/* CLOSE BUTTON */}
+              <img
+                src={closeIcon}
+                onClick={() => setShowLightbox(false)}
+                className="absolute top-23 right-[34%] w-5 cursor-pointer hover:scale-110 hover:bg-orange-500"
+              />
 
-          {/* Previous button */}
-          <img
-            src={prev}
-            alt="prevIcon"
-            onClick={() =>
-              setSelectedIndex((prev) =>
-                prev === 0 ? bigImages.length - 1 : prev - 1
-              )
-            }
-            className="absolute left-148 top-90 bg-white rounded-full w-12 h-12 p-4 cursor-pointer
-    "
-          />
-
-          {/* Image */}
-          <img
-            src={bigImages[selectedIndex]}
-            className="w-[500px] h-[500px] rounded-xl"
-          />
-
-          {/* Next button */}
-          <img
-            src={next}
-            alt="next-icon"
-            onClick={() =>
-              setSelectedIndex((prev) =>
-                prev === bigImages.length - 1 ? 0 : prev + 1
-              )
-            }
-            className="absolute right-148 top-90 bg-white rounded-full w-12 h-12 p-4  cursor-pointer"
-          />
-          <div className="flex mt-5 gap-5">
-            {thumbnails.map((thumb, index) => (
-              <div
-                key={index}
-                onClick={() => {
-                  setMainImage(bigImages[index]);
-                  setSelectedIndex(index);
-                }}
-                className={`rounded-lg w-22 h-20 cursor-pointer  transition-all duration-200
-        ${
-          selectedIndex === index
-            ? "border-2 border-orange-500 bg-gray-200"
-            : "hover:bg-gray-200 hover:border-2 hover:border-orange-500"
-        }`}
+              {/* PREV BUTTON */}
+              <button
+                onClick={() =>
+                  setSelectedIndex((prev) =>
+                    prev === 0 ? images.length - 1 : prev - 1
+                  )
+                }
+                className="absolute left-[34.5%] top-[390px] -translate-y-1/2 cursor-pointer bg-white w-12 h-12 rounded-full flex items-center justify-center"
               >
+                <img src={prev} className="w-3" />
+              </button>
+
+              {/* IMAGE */}
+              <div className="flex flex-col items-center">
                 <img
-                  src={thumb}
-                  className={`w-full h-full object-cover rounded-lg ${
-                    selectedIndex === index ? "opacity-50" : ""
-                  }`}
+                  src={images[selectedIndex]}
+                  className="w-[500px] h-[500px] rounded-xl"
                 />
+
+                {/* THUMBNAILS */}
+                <div className="flex gap-8  mt-3  ">
+                  {thumbnails.map((thumb, index) => (
+                    <div
+                      key={index}
+                      onClick={() => setSelectedIndex(index)}
+                      className={`w-20 h-20 rounded-lg cursor-pointer overflow-hidden border-2
+              ${
+                selectedIndex === index
+                  ? "border-orange-500"
+                  : "border-transparent"
+              }`}
+                    >
+                      <img
+                        src={thumb}
+                        className={`w-full h-full object-cover ${
+                          selectedIndex === index ? "opacity-50" : ""
+                        }`}
+                      />
+                    </div>
+                  ))}
+                </div>
               </div>
-            ))}
-          </div>
+
+              {/* NEXT BUTTON */}
+              <button
+                onClick={() =>
+                  setSelectedIndex((prev) =>
+                    prev === images.length - 1 ? 0 : prev + 1
+                  )
+                }
+                className="absolute right-[34.5%] top-[390px] -translate-y-1/2 cursor-pointer bg-white w-12 h-12 rounded-full flex items-center justify-center"
+              >
+                <img src={next} className="w-3" />
+              </button>
+            </div>
+          )}
+
+          {/* MOBILE ARROWS */}
+          <button
+            onClick={() =>
+              setSelectedIndex((prev) =>
+                prev === 0 ? images.length - 1 : prev - 1
+              )
+            }
+            className="absolute left-4 top-1/2 -translate-y-1/2 bg-white w-10 h-10 rounded-full flex items-center justify-center md:hidden"
+          >
+            <img src={prev} className="w-3" />
+          </button>
+
+          <button
+            onClick={() =>
+              setSelectedIndex((prev) =>
+                prev === images.length - 1 ? 0 : prev + 1
+              )
+            }
+            className="absolute right-4 top-1/2 -translate-y-1/2 bg-white w-10 h-10 rounded-full flex items-center justify-center md:hidden"
+          >
+            <img src={next} className="w-3" />
+          </button>
         </div>
-        
-      )}
-      
-      <div className="flex  flex-col mt-25 gap-3">
-        <h2 className="uppercase font-bold text-[hsl(219,9%,45%)]">
+
+        {/* DESKTOP THUMBNAILS */}
+        <div className="hidden md:flex gap-8 mt-4  ">
+          {thumbnails.map((thumb, index) => (
+            <div
+              key={index}
+              onClick={() => setSelectedIndex(index)}
+              className={`w-20 h-20 rounded-lg cursor-pointer overflow-hidden border-2 transition
+                ${
+                  selectedIndex === index
+                    ? "border-orange-500"
+                    : "border-transparent hover:border-orange-300"
+                }`}
+            >
+              <img
+                src={thumb}
+                className={`w-full h-full object-cover ${
+                  selectedIndex === index ? "opacity-50" : ""
+                }`}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* RIGHT SIDE (CONTENT) */}
+      <div className="w-full md:w-1/2 flex flex-col gap-5 px-6 md:px-0 md:mt-15">
+        <h2 className="uppercase text-gray-500 font-bold text-sm">
           Sneaker Company
         </h2>
-        <h1 className="text-black font-bold w-[560px] text-5xl  ">
+
+        <h1 className="text-2xl md:text-5xl font-bold leading-tight">
           Fall Limited Edition Sneakers
         </h1>
-        <p className="text-[hsl(219,9%,45%)] w-[450px] mt-7 gap-4 ">
+
+        <p className="text-gray-500 text-sm w-[430px] md:text-base">
           These low-profile sneakers are your perfect casual wear companion.
           Featuring a durable rubber outer sole, they’ll withstand everything
           the weather can offer.
         </p>
-        <span className="font-black text-2xl">
-          $125.00{" "}
-          <span className="bg-black text-white ml-6 p-1 text-lg">50%</span>
-        </span>
-        <span className="font-bold text-[hsl(219,9%,45%)] line-through">
-          $250.00
-        </span>
 
-        <div className="flex gap-2">
-          <div className="flex items-center justify-between bg-[hsl(220,20%,94%)] rounded-md w-24 h-8 px-2">
+        {/* PRICE */}
+        <div className="flex justify-between md:block">
+          <div className="flex items-center gap-4">
+            <span className="text-2xl font-bold">$125.00</span>
+            <span className="bg-black text-white px-2 py-1 text-sm rounded">
+              50%
+            </span>
+          </div>
+
+          <span className="line-through text-gray-500 font-bold md:block">
+            $250.00
+          </span>
+        </div>
+
+        {/* CART SECTION */}
+        <div className="flex flex-col md:flex-row gap-4 mt-4">
+          {/* QUANTITY */}
+          <div className="flex items-center justify-between bg-gray-100 px-4 py-3 rounded-lg md:w-40">
             <img
               src={minus}
-              alt="minus"
-              className="w-2 cursor-pointer"
               onClick={() => setQuantity(Math.max(0, quantity - 1))}
+              className="w-3 cursor-pointer"
             />
-
-            <input
-              type="number"
-              value={quantity}
-              onChange={(e) => setQuantity(Number(e.target.value))}
-              className="w-8 ml-5"
-            />
-
+            <span>{quantity}</span>
             <img
               src={plus}
-              alt="plus"
-              className="w-2 cursor-pointer"
               onClick={() => setQuantity(quantity + 1)}
+              className="w-3 cursor-pointer"
             />
           </div>
 
-          <div className="relative flex ">
-            <button
-              onClick={() => {
-                if (quantity > 0) {
-                  setCartQuantity(quantity); // send data to Navbar
-                }
-              }}
-              className="bg-[hsl(26,100%,55%)] cursor-pointer font-black rounded-lg w-[220px] h-[37px] font-bold text-[14px] text-black"
-            >
-              <img
-                src={cart}
-                alt="cart-icon"
-                className="w-[32px]  h-[26px]  p-2 ml-10 absolute  "
-              />
-              Add to cart
-            </button>
-          </div>
-        </div>
-        <div className="ml-[-530px] flex cursor-pointer  gap-5">
-          <div className="flex gap-5">
-            {thumbnails.map((thumb, index) => (
-              <div
-                key={index}
-                onClick={() => {
-                  setMainImage(bigImages[index]);
-                  setSelectedIndex(index);
-                }}
-                className={`rounded-lg w-22 h-20 cursor-pointer  transition-all duration-200
-        ${
-          selectedIndex === index
-            ? "border-2 border-orange-500 bg-gray-200"
-            : "hover:bg-gray-200 hover:border-2 hover:border-orange-500"
-        }`}
-              >
-                <img
-                  src={thumb}
-                  className={`w-full h-full object-cover rounded-lg ${
-                    selectedIndex === index ? "opacity-50" : ""
-                  }`}
-                />
-              </div>
-            ))}
-          </div>
+          {/* ADD TO CART */}
+          <button
+            onClick={() => quantity > 0 && setCartQuantity(quantity)}
+            className="bg-orange-500 w-full md:w-[250px] py-3 rounded-lg font-bold flex items-center justify-center gap-3"
+          >
+            <img src={cart} className="w-5" />
+            Add to cart
+          </button>
         </div>
       </div>
     </div>
